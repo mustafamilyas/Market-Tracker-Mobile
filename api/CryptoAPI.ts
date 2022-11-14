a4791e75dc8d7de49374b74c4c8b53b367578ed7
mustafamilyas <emailpribadiilyas@gmail.com>
2022-11-14 11:30:54 +0700

    feat: add response type

--- api/CryptoAPI.ts
+++ api/CryptoAPI.ts
@@ -1,11 +1,13 @@
+import { PriceChange, SupportedCurrencyGroup } from "./interfaces";
+
 export class CryptoAPI {
   static baseAPI = `https://api.pintu.co.id/v2`
 
-  static async getSupportedCurrencies() {
-    return fetch(this.baseAPI + '/wallet/supportedCurrencies').then(res => res.json());
+  static async getSupportedCurrencies(): Promise<SupportedCurrencyGroup[]>{
+    return fetch(this.baseAPI + '/wallet/supportedCurrencies').then(res => res.json()).then(res => res.payload);
   }
 
-  static async getPriceChanges() {
-    return fetch(this.baseAPI + '/trade/price-changes').then(res => res.json());
+  static async getPriceChanges() : Promise<PriceChange[]>{
+    return fetch(this.baseAPI + '/trade/price-changes').then(res => res.json()).then(res => res.payload);
   }
 }
---
+++ api/interfaces.ts
@@ -0,0 +1,31 @@
+export interface PriceChange {
+  pair: string;
+  latestPrice: string;
+  day: string | null;
+  week: string | null;
+  month: string | null;
+  year: string | null;
+}
+
+export interface SupportedCurrencyGroup {
+  currencyGroup: string;
+  color: string;
+  currencySymbol: string;
+  name: string;
+  logo: string;
+  decimal_point: number;
+  listingDate: string;
+  wallet: SupportedCurrency[]
+}
+
+export interface SupportedCurrency { 
+  currencyGroup: string;
+  tokenSymbol: string;
+  decimal_point: number;
+  tokenType: string;
+  blockchain: string;
+  explorer: string;
+  listingDate: string;
+  blockchainName: string;
+  logo: string
+}
